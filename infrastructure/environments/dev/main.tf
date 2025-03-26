38bef618d1177442f7d3a539219f67fe78c29ef4
Dewey Jose <richard.e.jose@gmail.com>
2025-03-26 08:56:57 -0400

    add a test bucket

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -54,18 +54,18 @@ module "github_oidc_role" {
   })
 }
 
-# resource "aws_s3_bucket" "tf_state_bucket" {
-#   bucket = "vibecheck-test-${var.env}"
-#   force_destroy = true
-#   tags = {
-#     env = var.env
-#   }
-# }
+resource "aws_s3_bucket" "tf_state_bucket" {
+  bucket = "vibecheck-test-${var.env}"
+  force_destroy = true
+  tags = {
+    env = var.env
+  }
+}
 
-# resource "aws_s3_bucket_versioning" "tf_state_bucket_versioning" {
-#   bucket = aws_s3_bucket.tf_state_bucket.id
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
+resource "aws_s3_bucket_versioning" "tf_state_bucket_versioning" {
+  bucket = aws_s3_bucket.tf_state_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
