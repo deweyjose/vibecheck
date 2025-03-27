94e60ce20d7f09ff92e5f3fe0ba5a6e325fa4bf0
Dewey Jose <richard.e.jose@gmail.com>
2025-03-26 23:48:48 -0400

    ase

--- infrastructure/environments/prod/main.tf
+++ infrastructure/environments/prod/main.tf
@@ -6,3 +6,12 @@ resource "aws_s3_bucket" "example" {
     env = var.env
   }
 }
+
+resource "aws_s3_bucket" "example" {
+  bucket = "vibecheck-test2-${var.env}"
+  force_destroy = true
+
+  tags = {    
+    env = var.env
+  }
+}
